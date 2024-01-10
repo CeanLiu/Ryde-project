d3f181a00555ff979c25994c1828ed1a25d7fdfb
CeanLiu <cean.liu320@gmail.com>
2024-01-10 13:51:37 -0500

    Update quicksetup.java

--- quicksetup.java
+++ quicksetup.java
@@ -1,6 +1,13 @@
 public class quicksetup {
     public static void main(String[] args) {
         System.out.println("you ded'");
+        jslkdfjalsdlaksjdfklasdjflk asdf
+        asdlkfjaksldfjkal sdf';
+        fg ajsd;fkgalds ;f
+        al's;g jkldfa
+        ;kfgj klhdfs
+        jg lkadl;
+        l fkgalds
     }
 }
     
