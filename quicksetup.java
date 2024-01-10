5f3e8e47d9d8dd24c048ba9be985a427281b6a22
zgl06 <zhangleo2006@gmail.com>
2024-01-10 13:53:22 -0500

    Update quicksetup.java

--- quicksetup.java
+++ quicksetup.java
@@ -1,5 +1,8 @@
 public class quicksetup {
     public static void main(String[] args) {
+        hello hello hello
+        hihih
+        ihihihi
         System.out.println("you ded'");
         jslkdfjalsdlaksjdfklasdjflk asdf
         asdlkfjaksldfjkal sdf';
